afe47ca01983beb52e9f04312c962207fbbffd4d
Gaurab@Lenovo.none <gaurabmdr@gmail.com>
2019-06-01 21:17:01 +0545

    Change virtual environment

--- deployment-scripts/gunicorn_django.sample.sh
+++ deployment-scripts/gunicorn_django.sample.sh
@@ -1,35 +1,32 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-NAME=mdps                                        # name of the application
-DJANGODIR=/home/server/project/mdps                        # django project directory
-SOCKFILE=/home/server/project/mdps/myvenv/run/gunicorn.sock  # we will communicte using this unix socket
-USER=server                                                # the user to run as
-GROUP=server                                               # the group to run as
-NUM_WORKERS=1                                                # how many worker processes should Gunicorn spaw
-DJANGO_SETTINGS_MODULE=mdps.settings             # which settings file should Django use
-DJANGO_WSGI_MODULE=mdps.wsgi                     # WSGI module name
-TIMEOUT=300                                                  # execution timeout (in second)
-
+NAME="mdps"                                   # Name of the application
+DJANGODIR=/var/lib/jenkins/workspace/MDPS               # Django project directory
+SOCKFILE=/var/lib/jenkins/workspace/MDPS/venv/run/gunicorn.sock  # we will communicte using this unix socket
+USER=jenkins                                         # the user to run as
+GROUP=jenkins                                        # the group to run as
+NUM_WORKERS=3                                       # how many worker processes should Gunicorn spawn
+DJANGO_SETTINGS_MODULE=mdps.settings      # which settings file should Django use
+DJANGO_WSGI_MODULE=mdps.wsgi              # WSGI module name
 echo "Starting $NAME as `whoami`"
 
-
 # Activate the virtual environment
+
 cd $DJANGODIR
-source /home/server/project/mdps/myvenv/bin/activate
+source /var/lib/jenkins/workspace/MDPS/venv/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
-
 # Create the run directory if it doesn't exist
+
 RUNDIR=$(dirname $SOCKFILE)
 test -d $RUNDIR || mkdir -p $RUNDIR
 
-
-# Start Django Unicorn
+# Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+
 exec gunicorn ${DJANGO_WSGI_MODULE}:application \
   --name $NAME \
-  --timeout $TIMEOUT \
   --workers $NUM_WORKERS \
   --user=$USER --group=$GROUP \
   --bind=unix:$SOCKFILE \
